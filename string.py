3b8bc28387f2e72fac00dc7f2329fa1ac39a1f4f
Chaitanya Waychal <chaitanya22607@gmail.com>
2026-08-01 14:10:13 +0000

    Reverse a String problem added

--- string.py
+++ string.py
@@ -1,5 +1,48 @@
-name = input("Enter a string: ")
-count = 0;
-for i in name:
-    count += 1
-print(count)
+# 1. String Length 
+ 
+# text = input("Enter a string: ")
+# count = 0;
+# for i in text:
+#     count += 1
+# print(count)
+
+# 2. Character Count  
+
+# text = input("Enter a string: ")
+# vowels = 0
+# consonants = 0
+# digits = 0
+# spaces = 0
+# special = 0
+
+# for ch in text:
+#     if ch.isalpha():
+#         if ch in "aeiouAEIOU":
+#             vowels += 1
+#         else:
+#             consonants += 1
+#     elif ch.isdigit():
+#         digits += 1
+#     elif ch == " ":
+#         spaces += 1
+#     else:
+#         special += 1
+
+# print("Vowels = ", vowels)
+# print("Consonants = ", consonants)
+# print("Digits = ", digits)
+# print("Spaces = ", spaces)
+# print("Special Characters = ", special)
+
+# 3. Reverse a String
+
+# text = input("Enter a string: ")
+# reverse = ""
+# for i in range( len(text) - 1, -1, -1):
+#     reverse += text[i]
+# print("Reversed string: ", reverse)
+
+# text = input("Enter a string: ")
+# print(text[::-1])
+
+# 4. Palindrome Check

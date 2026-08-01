4272604cd08d48b0bf9cd37eee03cf0f969effbb
Chaitanya Waychal <chaitanya22607@gmail.com>
2026-08-01 15:11:29 +0000

    adding some problems

--- string.py
+++ string.py
@@ -6,6 +6,7 @@
 #     count += 1
 # print(count)
 
+# --------------------------------------------------------------------------------------------------
 # 2. Character Count  
 
 # text = input("Enter a string: ")
@@ -34,15 +35,92 @@
 # print("Spaces = ", spaces)
 # print("Special Characters = ", special)
 
+# --------------------------------------------------------------------------------------------------
 # 3. Reverse a String
 
 # text = input("Enter a string: ")
 # reverse = ""
-# for i in range( len(text) - 1, -1, -1):
+# for i in range( len(text) - 1, -1, -1 ):
 #     reverse += text[i]
 # print("Reversed string: ", reverse)
 
 # text = input("Enter a string: ")
 # print(text[::-1])
 
+# --------------------------------------------------------------------------------------------------
 # 4. Palindrome Check
+
+# text = input("Enter a string: ")
+# reverse = ""
+# for i in range ( len(text) - 1, -1, -1 ):
+#     reverse += text[i]
+# if text == reverse:
+#     print("Palindrom")
+# else:
+#     print("Not a palindrome.")
+
+# text = input("Enter a string: ")
+# if text == text[::-1]:
+#     print("Palindrome")
+# else:
+#     print("Not a palindrome")
+
+# --------------------------------------------------------------------------------------------------
+# 5. Uppercase and Lowercase Count
+
+# text = input("Enter a string: ")
+# uppercase = 0
+# lowercase = 0
+# for ch in text:
+#     if ch.isupper():
+#         uppercase += 1
+#     elif ch.islower():
+#         lowercase += 1
+# print("Uppercase: ", uppercase)
+# print("Lowercase: ", lowercase)
+
+# if 'A' <= ch <= 'Z': uppercase += 1
+# elif 'a' <= ch <= 'z': lowercase += 1
+
+# --------------------------------------------------------------------------------------------------
+# 6. Replace Characters  
+
+# text = input("Enter a string: ")
+# org = input("Enter character to replace: ")
+# new = input("Enter new character: ")
+# result = ""
+# for ch in text:
+#     if ch == org:
+#         result += new
+#     else:
+#         result += ch
+# print("New string: ", result)
+
+# --------------------------------------------------------------------------------------------------
+# 7. Remove Spaces  
+
+# text = input("Enter a string: ")
+# result = ""
+# for ch in text:
+#     if ch != " ":
+#         result += ch
+# print("String without space:",result)
+
+# text = input("Enter a string: ")
+# print(text.replace(" ", ""))
+
+# --------------------------------------------------------------------------------------------------
+# 8. Frequency of a Character
+text = input("Enter a string: ")
+char = input("Enter a character")
+count = 0
+for ch in text:
+
+# --------------------------------------------------------------------------------------------------
+# 9. 
+
+
+# --------------------------------------------------------------------------------------------------
+# 10. 
+
+# --------------------------------------------------------------------------------------------------

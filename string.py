b115487c12a36d5375cb518763b84c7ee5283991
Chaitanya Waychal <chaitanya22607@gmail.com>
2026-08-04 15:08:44 +0000

    add 30 string manipulation problems with concepts and patterns

--- string.py
+++ string.py
@@ -316,3 +316,227 @@
 # Validate a password based on these conditions:  
 # Minimum 8 characters, At least one uppercase letter, One lowercase letter, One digit, One special character 
 
+# password = input("Enter password: ")
+# has_upper = False
+# has_lower = False
+# has_digit = False
+# has_special = False
+# if len(password) < 8:
+#     print("Invalid Password")
+# else:
+#     for ch in password:
+#         if ch.isupper():
+#             has_upper = True
+#         elif ch.islower():
+#             has_lower = True
+#         elif ch.isdigit():
+#             has_digit = True
+#         else:
+#             has_special = True
+#     if has_upper and has_lower and has_digit and has_special:
+#         print("Valid Password")
+#     else:
+#         print("Invalid Password")
+
+# --------------------------------------------------------------------------------------------------
+# 22. Run-Length Encoding
+# Compress a string by counting consecutive repeated characters.
+# Example: Input: aaabbccccd Output: a3b2c4d1
+
+# text = input("Enter a string: ")
+# count = 1
+# for i in range(len(text) - 1):
+#     if text[i] == text[i+1]:
+#         count += 1
+#     else:
+#         print(text[i] + str(count), end="")
+#         count = 1
+# print(text[-1] + str(count))
+
+# --------------------------------------------------------------------------------------------------
+# 23. String Compression  
+# Compress repeated characters and return the original string if compression does not reduce the length.
+
+# text = input("Entr a string: ")
+# compressed = ""
+# count = 1
+# for i in range(len(text) - 1):
+#     if text[i] == text[i + 1]:
+#         count += 1
+#     else:
+#         compressed += text[i] + str(count)
+#         count = 1
+# compressed += text[-1] + str(count)
+# if len(compressed) < len(text):
+#     print("Compressed String:", compressed)
+# else:
+#     print("Original String:", text)
+
+# --------------------------------------------------------------------------------------------------
+# 24. Most Frequent Character  
+# Find the character with the highest frequency.
+
+# text = input("Enter a string: ")
+# printed = ""
+# max_count = 0
+# max_char = ""
+# for ch in text:
+#     if ch not in printed:
+#         count = 0
+#         for c in text:
+#             if ch == c:
+#                 count += 1
+#         if count > max_count:
+#             max_count = count
+#             max_char = ch
+#         printed += ch
+# print("Most Frequent Character:", max_char)
+# print("Frequency:", max_count)
+
+# text = input("Enter a string: ")
+# freq = {}
+# for ch in text:
+#     freq[ch] = freq.get(ch, 0) + 1
+# max_char = ""
+# max_count = 0
+# for ch in freq:
+#     if freq[ch] > max_count:
+#         max_count = freq[ch]
+#         max_char = ch
+# print("Enter Frequent Character:", max_char)
+# print("Frequency:", max_count)
+
+# --------------------------------------------------------------------------------------------------
+# 25. Second Most Frequent Character  
+# Find the second most frequently occurring character.
+
+# text = input("Enter a string: ")
+# printed =""
+# first_count = 0
+# first_char = ""
+# second_count = 0
+# second_char = ""
+# for ch in text:
+#     if ch not in printed:
+#         count = 0
+#         for c in text:
+#             if ch == c:
+#                 count += 1
+#         if count > first_count:
+#             second_count = first_count
+#             second_char = first_char
+#             first_count = count
+#             first_char = ch
+#         elif count > second_count:
+#             second_count = count
+#             second_char = ch
+#         printed += ch
+# print("Most Frequent Character: ", first_char)
+# print("Second Most Frequent: ", second_char)
+# print("Frequency: ", second_count)
+
+# --------------------------------------------------------------------------------------------------
+# 26. Caesar Cipher  
+# Encrypt and decrypt a message using the Caesar Cipher algorithm.
+
+# Encryption Program
+
+# message = input("Enter message:")
+# shift = int(input("Enter shift: "))
+# encrypted = ""
+# for ch in message:
+#     if ch.isupper():
+#         encrypted += chr((ord(ch) - 65 + shift) % 26 + 65)
+#     elif ch.islower():
+#         encrypted += chr((ord(ch) - 97 + shift) % 26 + 97)
+#     else:
+#         encrypted += ch
+# print("Encrypted Mesage:", encrypted)
+
+# Decryption Program
+
+# message = input("Enter encrypted message: ")
+# shift = int(input("Enter shift: "))
+# decrypted = ""
+# for ch in message:
+#     if ch.isupper():
+#         decrypted += chr((ord(ch) - 65 - shift) % 26 + 65)
+#     elif ch.islower():
+#         decrypted += chr((ord(ch) - 97 - shift) % 26 + 97)
+#     else:
+#         decrypted += ch
+# print("Decrypted message: ", decrypted)
+
+# --------------------------------------------------------------------------------------------------
+# 27. Email Validator  
+# Validate whether a given email address follows a valid format. 
+
+# email = input("Enter email: ")
+# if email.count("@") != 1:
+#     print("Invalid Email")
+# else:
+#     parts = email.split("@")
+#     username = parts[0]
+#     domain = parts[1]
+#     if username == "":
+#         print("Invalid Email")
+#     elif domain == "":
+#         print("Invalis Email")
+#     elif "." not in domain:
+#         print("Invalis Email")
+#     else:
+#         print("Valid Email")
+
+# import re
+# email = input("Enter email: ")
+# pattern = r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'
+# if re.match(pattern, email):
+#     print("Valid Email")
+# else:
+#     print("Invalid Email")
+
+# --------------------------------------------------------------------------------------------------
+# 28. Word Frequency Dictionary  
+# Count the frequency of every word in a paragraph.
+
+# paragraph = input("Enter a paragraph: ")
+# words = paragraph.split()
+# freq = {}
+# for word in words:
+#     if word in freq:
+#         freq[word] += 1
+#     else:
+#         freq[word] = 1
+# print("Word Frequencies")
+# for word in freq:
+#     print(word, ":", freq[word])
+
+# --------------------------------------------------------------------------------------------------
+# 29. Sentence Reversal  
+# Reverse the order of words in a sentence without changing the words themselves.   
+
+# sentence = input("Enter a sentence: ")
+# words = sentence.split()
+# for i in range(len(words)-1, -1, -1):
+#     print(words[i], end=" ")
+
+# sentence = input("Enter a sentence: ")
+# words = sentence.split()
+# print(" ".join(words[::-1]))
+
+# --------------------------------------------------------------------------------------------------
+# 30. String Rotation  
+# Check whether one string is a rotation of another.
+
+# s1 = input("Enter first string: ")
+# s2 = input("Enter second string: ")
+# if len(s1) != len(s2):
+#     print("No")
+# else:
+#     temp = s1 + s2
+#     if s2 in temp:
+#         print("Yes")
+#     else:
+#         print("No")
+
+# --------------------------------------------------------------------------------------------------
